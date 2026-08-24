b569830884bcde799178f2e1ae0dee5cc27656f9
rajtompe059090-create <rajtompe059090@gmail.com>
2026-08-24 22:17:21 +0530

    Refactor MainActivity to simplify navigation and state

--- android/app/src/main/java/com/arrowescape/game/MainActivity.kt
+++ android/app/src/main/java/com/arrowescape/game/MainActivity.kt
@@ -1,149 +1,37 @@
-package com.arrowescape.game
+AppScreen.GAME -> {
+    GameScreen(
+        uiState = uiState,
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.arrowescape.game.data.UserPreferencesRepository
-import com.arrowescape.game.ui.screens.GameScreen
-import com.arrowescape.game.ui.screens.HomeScreen
-import com.arrowescape.game.ui.screens.LevelSelectScreen
-import com.arrowescape.game.ui.screens.SplashScreen
-import com.arrowescape.game.ui.theme.ArrowEscapeTheme
-import com.arrowescape.game.viewmodel.GameViewModel
+        onArrowTapped = { arrow ->
+            viewModel.onArrowTapped(arrow)
+        },
 
-enum class AppScreen {
-    SPLASH,
-    HOME,
-    LEVEL_SELECT,
-    GAME
-}
+        onUseHint = {
+            viewModel.useHint()
+        },
 
-class MainActivity : ComponentActivity() {
+        onRestartLevel = {
+            viewModel.restartCurrentLevel()
+        },
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        onOpenLevels = {
+            currentScreen = AppScreen.LEVEL_SELECT
+        },
 
-        enableEdgeToEdge()
+        onBack = {
+            currentScreen = AppScreen.LEVEL_SELECT
+        },
 
-        setContent {
-            ArrowEscapeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val prefsRepo = remember {
-                        UserPreferencesRepository(applicationContext)
-                    }
+        onNextLevel = {
+            val currentLevelId =
+                uiState.currentLevel?.id ?: 1
 
-                    val gameViewModel: GameViewModel = viewModel {
-                        GameViewModel(prefsRepo)
-                    }
+            val nextLevelId =
+                currentLevelId + 1
 
-                    ArrowEscapeApp(gameViewModel)
-                }
-            }
+            viewModel.loadLevel(nextLevelId)
+
+            currentScreen = AppScreen.GAME
         }
-    }
-}
-
-@Composable
-fun ArrowEscapeApp(
-    viewModel: GameViewModel
-) {
-    val uiState by viewModel.uiState.collectAsState()
-
-    var currentScreen by remember {
-        mutableStateOf(AppScreen.SPLASH)
-    }
-
-    when (currentScreen) {
-
-        AppScreen.SPLASH -> {
-            SplashScreen(
-                onStart = {
-                    currentScreen = AppScreen.HOME
-                }
-            )
-        }
-
-        AppScreen.HOME -> {
-            HomeScreen(
-                uiState = uiState,
-
-                onContinueGame = {
-                    viewModel.loadLevel(uiState.unlockedLevel)
-                    currentScreen = AppScreen.GAME
-                },
-
-                onSelectLevels = {
-                    currentScreen = AppScreen.LEVEL_SELECT
-                },
-
-                onOpenWallet = {
-                    // Wallet screen can be added later
-                },
-
-                onToggleSound = {
-                    viewModel.toggleSound()
-                }
-            )
-        }
-
-        AppScreen.LEVEL_SELECT -> {
-            LevelSelectScreen(
-                uiState = uiState,
-
-                onSelectLevel = { levelId ->
-                    viewModel.loadLevel(levelId)
-                    currentScreen = AppScreen.GAME
-                },
-
-                onBack = {
-                    currentScreen = AppScreen.HOME
-                }
-            )
-        }
-
-        AppScreen.GAME -> {
-            GameScreen(
-                uiState = uiState,
-
-                onArrowTapped = { arrow ->
-                    viewModel.onArrowTapped(arrow)
-                },
-
-                onUseHint = {
-                    viewModel.useHint()
-                },
-
-                onRestartLevel = {
-                    viewModel.restartCurrentLevel()
-                },
-
-                onOpenLevels = {
-                    currentScreen = AppScreen.LEVEL_SELECT
-                },
-
-                onBack = {
-                    currentScreen = AppScreen.LEVEL_SELECT
-                },
-
-                onNextLevel = {
-                    viewModel.loadLevel(uiState.unlockedLevel)
-                }
-            )
-        }
-    }
+    )
 }
