1dacd69bee1c06468512702fd616a9d1d3f71c7e
rajtompe059090-create <rajtompe059090@gmail.com>
2026-08-24 22:35:29 +0530

    Refactor MainActivity to use Jetpack Compose

--- android/app/src/main/java/com/arrowescape/game/MainActivity.kt
+++ android/app/src/main/java/com/arrowescape/game/MainActivity.kt
@@ -1,33 +1,85 @@
-AppScreen.GAME -> {
-    GameScreen(
-        uiState = uiState,
+package com.arrowescape.game
 
-        onArrowTapped = { arrow ->
-            viewModel.onArrowTapped(arrow)
-        },
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 
-        onUseHint = {
-            viewModel.useHint()
-        },
+class MainActivity : ComponentActivity() {
 
-        onRestartLevel = {
-            viewModel.restartCurrentLevel()
-        },
+    private val viewModel: GameViewModel by viewModels()
 
-        onOpenLevels = {
-            currentScreen = AppScreen.LEVEL_SELECT
-        },
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
-        onBack = {
-            currentScreen = AppScreen.LEVEL_SELECT
-        },
+        setContent {
+            val uiState = viewModel.uiState
+            var currentScreen by remember {
+                mutableStateOf(AppScreen.LEVEL_SELECT)
+            }
 
-        onNextLevel = {
-            val currentLevelId = uiState.currentLevel?.id ?: 1
-            val nextLevelId = currentLevelId + 1
+            when (currentScreen) {
 
-            viewModel.loadLevel(nextLevelId)
-            currentScreen = AppScreen.GAME
+                AppScreen.LEVEL_SELECT -> {
+                    LevelSelectScreen(
+                        uiState = uiState,
+
+                        onLevelSelected = { levelId ->
+                            viewModel.loadLevel(levelId)
+                            currentScreen = AppScreen.GAME
+                        },
+
+                        onBack = {
+                            finish()
+                        }
+                    )
+                }
+
+                AppScreen.GAME -> {
+                    GameScreen(
+                        uiState = uiState,
+
+                        onArrowTapped = { arrow ->
+                            viewModel.onArrowTapped(arrow)
+                        },
+
+                        onUseHint = {
+                            viewModel.useHint()
+                        },
+
+                        onRestartLevel = {
+                            viewModel.restartCurrentLevel()
+                        },
+
+                        onOpenLevels = {
+                            currentScreen = AppScreen.LEVEL_SELECT
+                        },
+
+                        onBack = {
+                            currentScreen = AppScreen.LEVEL_SELECT
+                        },
+
+                        onNextLevel = {
+                            val currentLevelId =
+                                uiState.currentLevel?.id ?: 1
+
+                            val nextLevelId =
+                                currentLevelId + 1
+
+                            viewModel.loadLevel(nextLevelId)
+                            currentScreen = AppScreen.GAME
+                        }
+                    )
+                }
+            }
         }
-    )
+    }
 }
