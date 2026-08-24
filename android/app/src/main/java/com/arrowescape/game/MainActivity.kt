44c46d064e2e0514e17376f74b3a0bea3913985f
rajtompe059090-create <rajtompe059090@gmail.com>
2026-08-24 22:02:52 +0530

    Refactor MainActivity UI and remove unused imports
    Refactor MainActivity to simplify UI structure and remove unused imports.

--- android/app/src/main/java/com/arrowescape/game/MainActivity.kt
+++ android/app/src/main/java/com/arrowescape/game/MainActivity.kt
@@ -4,29 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdSize
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
-import androidx.compose.ui.viewinterop.AndroidView
 import com.arrowescape.game.data.UserPreferencesRepository
 import com.arrowescape.game.ui.screens.GameScreen
 import com.arrowescape.game.ui.screens.HomeScreen
@@ -49,21 +37,21 @@ class MainActivity : ComponentActivity() {
 
         enableEdgeToEdge()
 
-        MobileAds.initialize(this) {}
-
         setContent {
             ArrowEscapeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val prefsRepo =
-                        remember { UserPreferencesRepository(applicationContext) }
+                    val prefsRepo = remember {
+                        UserPreferencesRepository(applicationContext)
+                    }
 
-                    val viewModel: GameViewModel =
-                        viewModel { GameViewModel(prefsRepo) }
+                    val gameViewModel: GameViewModel = viewModel {
+                        GameViewModel(prefsRepo)
+                    }
 
-                    ArrowEscapeApp(viewModel)
+                    ArrowEscapeApp(gameViewModel)
                 }
             }
         }
@@ -91,40 +79,37 @@ fun ArrowEscapeApp(
         }
 
         AppScreen.HOME -> {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
+            HomeScreen(
+                uiState = uiState,
 
-                HomeScreen(
-                    uiState = uiState,
-                    onContinueGame = {
-                        viewModel.loadLevel(uiState.unlockedLevel)
-                        currentScreen = AppScreen.GAME
-                    },
-                    onSelectLevels = {
-                        currentScreen = AppScreen.LEVEL_SELECT
-                    },
-                    onOpenWallet = {
-                    },
-                    onToggleSound = {
-                        viewModel.toggleSound()
-                    },
-                    modifier = Modifier.weight(1f)
-                )
+                onContinueGame = {
+                    viewModel.loadLevel(uiState.unlockedLevel)
+                    currentScreen = AppScreen.GAME
+                },
 
-                TestBannerAd()
-            }
+                onSelectLevels = {
+                    currentScreen = AppScreen.LEVEL_SELECT
+                },
+
+                onOpenWallet = {
+                    // Wallet screen can be added later
+                },
+
+                onToggleSound = {
+                    viewModel.toggleSound()
+                }
+            )
         }
 
         AppScreen.LEVEL_SELECT -> {
             LevelSelectScreen(
                 uiState = uiState,
+
                 onSelectLevel = { levelId ->
                     viewModel.loadLevel(levelId)
                     currentScreen = AppScreen.GAME
                 },
+
                 onBack = {
                     currentScreen = AppScreen.HOME
                 }
@@ -132,58 +117,33 @@ fun ArrowEscapeApp(
         }
 
         AppScreen.GAME -> {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
+            GameScreen(
+                uiState = uiState,
 
-                GameScreen(
-                    modifier = Modifier.weight(1f),
-                    uiState = uiState,
-                    onArrowTapped = { arrow ->
-                        viewModel.onArrowTapped(arrow)
-                    },
-                    onUseHint = {
-                        viewModel.useHint()
-                    },
-                    onRestartLevel = {
-                        viewModel.restartCurrentLevel()
-                    },
-                    onOpenLevels = {
-                        currentScreen = AppScreen.LEVEL_SELECT
-                    },
-                    onBack = {
-                        currentScreen = AppScreen.LEVEL_SELECT
-                    }
-                )
+                onArrowTapped = { arrow ->
+                    viewModel.onArrowTapped(arrow)
+                },
 
-                TestBannerAd()
-            }
+                onUseHint = {
+                    viewModel.useHint()
+                },
+
+                onRestartLevel = {
+                    viewModel.restartCurrentLevel()
+                },
+
+                onOpenLevels = {
+                    currentScreen = AppScreen.LEVEL_SELECT
+                },
+
+                onBack = {
+                    currentScreen = AppScreen.LEVEL_SELECT
+                },
+
+                onNextLevel = {
+                    viewModel.loadLevel(uiState.unlockedLevel)
+                }
+            )
         }
     }
-}
-
-@Composable
-fun TestBannerAd() {
-
-    AndroidView(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 4.dp),
-
-        factory = { context ->
-
-            AdView(context).apply {
-
-                setAdSize(AdSize.BANNER)
-
-                adUnitId =
-                    "ca-app-pub-3940256099942544/9214589741"
-
-                loadAd(
-                    AdRequest.Builder().build()
-                )
-            }
-        }
-    )
 }
