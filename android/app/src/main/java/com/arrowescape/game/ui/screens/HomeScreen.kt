758cb6709212b0d02b09e6acf4660d38e48016a3
rajtompe059090-create <rajtompe059090@gmail.com>
2026-08-27 22:34:38 +0530

    refactor(ui): remove unused imports in HomeScreen

--- android/app/src/main/java/com/arrowescape/game/ui/screens/HomeScreen.kt
+++ android/app/src/main/java/com/arrowescape/game/ui/screens/HomeScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -43,7 +42,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
