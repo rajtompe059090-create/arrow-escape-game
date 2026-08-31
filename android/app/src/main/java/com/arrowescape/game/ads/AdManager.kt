2da7e7473fe2c399b32233962edbcd8da8aacea4
rajtompe059090-create <rajtompe059090@gmail.com>
2026-08-31 18:49:53 +0530

    feat(ads): add wrapContentSize to AdManager layout

--- android/app/src/main/java/com/arrowescape/game/ads/AdManager.kt
+++ android/app/src/main/java/com/arrowescape/game/ads/AdManager.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
